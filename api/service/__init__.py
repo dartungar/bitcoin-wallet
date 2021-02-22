060515740b46332e7a8bb43ba4229b880a0d8b4d
dartungar <dartungar@gmail.com>
2021-02-22 18:50:37 +0300

    api v0.1

--- api/service/__init__.py
+++ api/service/__init__.py
@@ -1,14 +1,16 @@
-from .models import Transaction
-from fastapi import FastAPI
-from pydantic import BaseModel
+from .models import Transaction, Address
 from .db import session
+from sqlalchemy import func
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from random import randint
-from typing import List
+from typing import List, Optional
+from bitcoinaddress import Wallet, Seed
 
 app = FastAPI()
 
 
-class TransactionResponse(BaseModel):
+class ITransaction(BaseModel):
     '''Pydantic model for response containing Transaction object'''
     id: str
     input: str
@@ -18,31 +20,90 @@ class TransactionResponse(BaseModel):
     timestamp: str
 
 
-@app.get("/api/transactions", response_model=List[TransactionResponse])
-async def get_transactions(address: str) -> List[TransactionResponse]:
+class IAddressData(BaseModel):
+    address: str
+    balance: float
+
+
+class ISeed(BaseModel):
+    seed: str
+
+
+@app.get("/api/transactions", response_model=List[ITransaction])
+async def get_transactions(address: str) -> List[ITransaction]:
     '''
     Get a list of transactions for a given address.
     '''
-    transactions_raw = session.query(Transaction).filter_by(input=address).union(
-        session.query(Transaction).filter_by(output=address)).all()
+    transactions_raw = session.query(Transaction).filter_by(input_address=address).union(
+        session.query(Transaction).filter_by(output_address=address)).all()
     if not transactions_raw:
         print('data not found, generating dummy data...')
         generate_dummy_transactions(address)
-        transactions_raw = session.query(Transaction).filter_by(input=address).union(
-            session.query(Transaction).filter_by(output=address)).all()
+        transactions_raw = session.query(Transaction).filter_by(input_address=address).union(
+            session.query(Transaction).filter_by(output_address=address)).all()
     transactions = [t.to_json() for t in transactions_raw]
     return transactions
 
 
-@app.get("/api/address")
+@app.get("/api/addresses", response_model=List[IAddressData])
 async def get_addresses(seed: str):
-    # TODO
-    pass
+    '''
+    get addresses and their balance for given seed. \n
+    seed should be encrypted in prod
+    '''
+    addresses = session.query(Address).filter_by(seed=seed).all()
+    if addresses:
+        addresses_data = []
+        for a in addresses:
+            balance_out = session.query(
+                func.sum(Transaction.amount)).filter(Transaction.input_address == a.address).first()[0]
+            balance_in = session.query(func.sum(Transaction.amount)).filter(
+                Transaction.output_address == a.address).first()[0]
+            print(f'in {balance_in}, out {balance_out}')
+            if not balance_out:
+                balance_out = 0
+            if not balance_in:
+                balance_in = 0
+            addresses_data.append(
+                {"address": a.address, "balance": a.initial_balance + balance_in - balance_out})
+        return addresses_data
+    raise HTTPException(status_code=404, detail="Error getting addresses")
 
 
 @app.post("/api/address")
-async def add_address(seed: str):
-    pass
+async def add_address(seed: ISeed):
+    '''
+    add new address and return its key
+    '''
+    # TO DO: pass seed in request body
+    # TO DO: проверить отличаются ли новые адреса для одного сида
+    print(seed)
+    key = generate_and_add_address(seed=seed.seed)
+    return key
+
+
+@app.get("/api/seed")
+async def generate_seed():
+    '''generate a new seed. \n 
+       use its private key \n
+       for prototyping only, use proper seed generation in prod 
+    '''
+    return Wallet().key.hex
+
+
+def generate_and_add_address(seed: Optional[str] = None):
+    '''
+    generate new address, random or for given seed.
+    '''
+    # this is no proper address generation
+    # but it is taking quite some time to get into bitcoin
+
+    wallet = Wallet()
+    key = wallet.address.pubkey
+    new_address = Address(address=key, seed=seed)
+    session.add(new_address)
+    session.commit()
+    return key
 
 
 def generate_dummy_transactions(address: str):
@@ -52,11 +113,11 @@ def generate_dummy_transactions(address: str):
     for i in range(randint(0, 10)):
         if i % 2:
             inp = address
-            outp = 'lol'
+            outp = generate_and_add_address()
         else:
-            inp = 'lol'
+            inp = generate_and_add_address()
             outp = address
         transaction = Transaction(
-            input=inp, output=outp, amount=randint(1, 1000), fee=randint(1, 7)/1000)
+            input_address=inp, output_address=outp, amount=randint(1, 1000), fee=randint(1, 7)/1000)
         session.add(transaction)
         session.commit()
--- api/service/models.py
+++ api/service/models.py
@@ -1,26 +1,40 @@
-from sqlalchemy import Column, String, Float, DateTime, DateTime
+from random import randint
+from sqlalchemy import Column, String, Float, DateTime, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 from uuid import uuid4
 from datetime import datetime
 from .db import Base, engine
-import json
 
 
 # transaction
 class Transaction(Base):
-    __tablename__ = "transactions"
+    __tablename__ = "transaction"
     id = Column(UUID(as_uuid=True), primary_key=True,
                 default=uuid4, unique=True, nullable=False)
-    input = Column(String, nullable=False)
-    output = Column(String, nullable=False)
+    # should've done relationship
+    # but multiple FKs are tricky
+    # works fine for prototype
+    input_address = Column(String, nullable=False)
+    output_address = Column(String, nullable=False)
     amount = Column(Float, nullable=False)
     fee = Column(Float, default=0.003, nullable=False)
     timestamp = Column(DateTime, default=datetime.now(), nullable=False)
 
     def to_json(self):
-        item_obj = {"id": str(self.id), "input": self.input, "output": self.output,
+        item_obj = {"id": str(self.id), "input": self.input_address, "output": self.output_address,
                     "amount": self.amount, "fee": self.fee, "timestamp": str(self.timestamp)}
         return item_obj
+
+
+# address
+class Address(Base):
+    __tablename__ = "address"
+    address = Column(String, primary_key=True, unique=True, nullable=False)
+    # should be hashed or otherwise protected in prod
+    seed = Column(String, default='dummy', nullable=False)
+    # for testing purposes only
+    initial_balance = Column(Float, nullable=False, default=10000)
 
 
 Base.metadata.create_all(engine)
